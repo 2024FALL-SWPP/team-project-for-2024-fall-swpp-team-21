9419bb34475abf644bee6ff711fa2d0bab318210
EsOTi <tpwls6630@naver.com>
2024-10-30 15:17:07 +0900

    fix : 8-axis movement
    issue(resolved) : threshold frame값에 0.2f를 곱해주지 않았던 문제 수정.

--- Computer Virus Survivors/Assets/Scripts/PlayerController.cs
+++ Computer Virus Survivors/Assets/Scripts/PlayerController.cs
@@ -18,7 +18,9 @@ public class PlayerController : MonoBehaviour
 
     void Move()
     {
-        const int thresholdFrame = 3; 
+        const int thresholdFrame = 3;       // 대각선으로 움직이다 한 쪽 방향키를 뗐을 때 대각선을 유지하는 유예 프레임
+        const float inputUnitPerSec = .2f;  // 방향키를 뗐을 때 axis input 감소량 (input manager -> gravity / 60)
+
 
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
@@ -26,18 +28,23 @@ public class PlayerController : MonoBehaviour
         float horizontalAbs = Mathf.Abs(horizontalInput);
         float verticalAbs = Mathf.Abs(verticalInput);
 
-        if(horizontalAbs < 0.6f && verticalAbs < 0.6f) {return;}
+        // 두 방향키를 모두 뗀 후 3프레임이 지난 시점에 중립 판정
+        if(horizontalAbs < .6f && verticalAbs < .6f) {return;}
 
-        if(horizontalAbs - verticalAbs > thresholdFrame + .1f)
+        // 8-axis movement
+        if(horizontalAbs - verticalAbs > thresholdFrame * inputUnitPerSec + .1f)
         {
+            // 수직 방향키를 뗀 후 3프레임 이상 지나면 수직 속력 0
             verticalInput = 0;
         }
-        else if(verticalAbs - horizontalAbs > thresholdFrame + .1f)
+        else if(verticalAbs - horizontalAbs > thresholdFrame * inputUnitPerSec + .1f)
         {
+            // 수평 방향키를 뗀 후 3프레임 이상 지나면 수평 속력 0
             horizontalInput = 0;
         }
         else
         {
+            // 각 축에 대해 가능한 속력을 {-1, 0, 1}로 제한하여 8축의 이산 방향 설정
             if(verticalAbs > .1f)
             {
                 verticalInput /= verticalAbs;
