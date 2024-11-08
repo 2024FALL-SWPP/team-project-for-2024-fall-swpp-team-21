655920cc461550a3733e620b9439bebf749137be
Nyso97 <justin0907@naver.com>
2024-11-08 21:03:23 +0900

    웨이브 파서
    - SpawnPattern 클래스 추가
    isSpawnPointRandom이 true인 경우, player로부터의 offset, 그 offset 더한 지점으로부터의 spawn range가 필요
    false인 경우 spawnPoints에 좌표를 list로 저장
    
    - SpawnManager의 Inspector 창에서 spawnPattern list 설정 & JSON으로 저장 & 로드 가능
    
    - spawnManager의 동작 수정
    게임 시작시 SpawnManager가 spawnPatterns.json(프로젝트 최상위 폴더에 있음)으로 부터 spawn pattern list 불러옴
    MainSpawnCoroutine에서 각 spawn pattern들을 순서대로 시작 시간에 맞게 코루틴으로 실행
    
    - Spawn 동작 일부 수정
    박스 범위 스폰 -> 원 범위 스폰
    숫자키 눌러서 spawn할 수 있는 기능은 유지시킴
    
    - GameManager에 게임 시간 public 변수 추가

--- Computer Virus Survivors/Assets/Scripts/SpawnManager.cs
+++ Computer Virus Survivors/Assets/Scripts/SpawnManager.cs
@@ -1,25 +1,69 @@
+using System;
+using System.IO;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
+
+[Serializable]
+public class SpawnPattern
+{
+    // 초 단위
+    public Vector2 spawnTimeRange;
+    public int spawnPeriod;
+    public PoolType virusID;
+    public int spawnMonsterNum;
+    public bool isSpawnPointRandom;
+
+    [Header("랜덤인 경우만")]
+    public Vector2 offsetFromPlayer;
+    public Vector2 spawnRange;
+
+    [Header("랜덤이 아닌 경우만 (spawnMonsterNum만큼의 spawn point가 있어야 함)")]
+    public List<Vector2> spawnPoints;
+}
+
+public class SpawnPatternList
+{
+    public List<SpawnPattern> patterns;
+
+    public static string CreateJSON(SpawnPatternList spawnPatternList)
+    {
+        return JsonUtility.ToJson(spawnPatternList);
+    }
+}
+
+
 
 public class SpawnManager : MonoBehaviour
 {
-    /* Temp version of SpawnManager*/
-
+    // for test: 플레이어 주위 스폰 범위
     [SerializeField] private Vector2 spawnRange;
+
     private GameObject player;
+    public List<SpawnPattern> spawnPatterns;
+    // private List<Coroutine> runningCoroutines = new List<Coroutine>();
 
     private void Start()
     {
         player = GameManager.instance.Player;
+        LoadSpawnPattern();
+        spawnPatterns.Sort((a, b) => a.spawnTimeRange.x.CompareTo(b.spawnTimeRange.x));
+        StartCoroutine(MainSpawnCoroutine());
     }
 
-    // Update is called once per frame
+
+    // for test: 플레이어 주위 스폰
     public void Spawn(PoolType index)
     {
-        float x = Random.Range(-spawnRange.x, spawnRange.x);
-        float z = Random.Range(-spawnRange.y, spawnRange.y);
+        Vector2 randomPoint = GetRandomPoint(spawnRange.x, spawnRange.y);
 
+        Spawn(index, randomPoint.x, randomPoint.y);
+    }
+
+    // (x, z) 위치에 바이러스 스폰
+    public void Spawn(PoolType index, float x, float z)
+    {
         Vector3 spawnPosition = player.transform.position + new Vector3(x, 0, z);
 
         GameObject virus = PoolManager.instance.GetObject
@@ -32,4 +76,123 @@ public class SpawnManager : MonoBehaviour
         virus.GetComponent<VirusBehaviour>().Initialize(player);
     }
 
+
+    // 메인 코루틴: spawn pattern 코루틴들을 순서대로 시작 시간에 맞춰 실행
+    private IEnumerator MainSpawnCoroutine()
+    {
+        foreach (SpawnPattern spawnPattern in spawnPatterns)
+        {
+            yield return new WaitUntil(() => GameManager.instance.gameTime >= spawnPattern.spawnTimeRange.x);
+            StartCoroutine(SpawnPatternCoroutine(spawnPattern));
+            //runningCoroutines.Add(StartCoroutine(SpawnPatternCoroutine(spawnPattern)));
+        }
+    }
+
+    // spawn pattern 코루틴: spawn pattern에 따라 바이러스 스폰
+    private IEnumerator SpawnPatternCoroutine(SpawnPattern spawnPattern)
+    {
+        PoolType virusPoolType = spawnPattern.virusID;
+        while (GameManager.instance.gameTime <= spawnPattern.spawnTimeRange.y)
+        {
+            for (int i = 0; i < spawnPattern.spawnMonsterNum; i++)
+            {
+                Vector2 randomPoint = spawnPattern.isSpawnPointRandom ?
+                    GetRandomPoint(spawnPattern.spawnRange.x, spawnPattern.spawnRange.y) :
+                    spawnPattern.spawnPoints[i];
+                float x = randomPoint.x + spawnPattern.offsetFromPlayer.x;
+                float z = randomPoint.y + spawnPattern.offsetFromPlayer.y;
+
+                Spawn(virusPoolType, x, z);
+            }
+            yield return new WaitForSeconds(spawnPattern.spawnPeriod);
+        }
+    }
+
+
+    // json 파일에서 spawn pattern list 로드
+    public void LoadSpawnPattern()
+    {
+        string jsonText = File.ReadAllText("spawnPatterns.json");  // path?
+        SpawnPatternList spawnPatternList = JsonUtility.FromJson<SpawnPatternList>(jsonText);
+        spawnPatterns = spawnPatternList.patterns;
+    }
+
+    // 반지름이 minRadius ~ maxRadius인 원 안의 랜덤한 점 반환
+    private Vector2 GetRandomPoint(float minRadius, float maxRadius)
+    {
+        // Ensure minRadius is not greater than maxRadius
+        if (minRadius > maxRadius)
+        {
+            float temp = minRadius;
+            minRadius = maxRadius;
+            maxRadius = temp;
+        }
+
+        // Random angle in radians
+        float angle = UnityEngine.Random.Range(0f, Mathf.PI * 2);
+
+        // Random radius between minRadius and maxRadius
+        float radius = UnityEngine.Random.Range(minRadius, maxRadius);
+
+        // Convert polar coordinates to Cartesian coordinates
+        float x = radius * Mathf.Cos(angle);
+        float y = radius * Mathf.Sin(angle);
+
+        return new Vector2(x, y);
+    }
+}
+
+
+
+/// <summary>
+/// JSON 파일로 저장 및 로드하는 에디터
+/// </summary>
+[CustomEditor(typeof(SpawnManager))]
+public class SpawnManagerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        if (GUILayout.Button("Save Spawn Patterns as JSON"))
+        {
+            SaveAsJson();
+        }
+
+        if (GUILayout.Button("Load Spawn Patterns from JSON"))
+        {
+            LoadFromJson();
+        }
+    }
+
+    private void SaveAsJson()
+    {
+        SpawnManager manager = (SpawnManager) target;
+
+        string jsonString = SpawnPatternList.CreateJSON(new SpawnPatternList { patterns = manager.spawnPatterns });
+        try
+        {
+            File.WriteAllText("spawnPatterns.json", jsonString);
+            Debug.Log("Successfully saved Spawn Patterns as JSON");
+        }
+        catch (Exception e)
+        {
+            Debug.Log("Failed to save Spawn Patterns as JSON: " + e.Message);
+        }
+    }
+
+    private void LoadFromJson()
+    {
+        SpawnManager manager = (SpawnManager) target;
+
+        try
+        {
+            manager.LoadSpawnPattern();
+            Debug.Log("Successfully loaded Spawn Patterns as JSON");
+        }
+        catch (Exception e)
+        {
+            Debug.Log("Failed to load Spawn Patterns as JSON: " + e.Message);
+        }
+    }
 }
