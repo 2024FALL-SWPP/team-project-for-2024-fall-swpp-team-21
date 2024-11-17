46e918a628f66ebf5ed580a8c6dba7f81afca6f9
EsOTi <tpwls6630@naver.com>
2024-11-17 22:52:50 +0900

    무기 레벨업 하면 코루틴 다시 시작하도록 수정 화염방사기 코루틴 수정 화염방사기 범위, 밀도 수정

--- Computer Virus Survivors/Assets/Scripts/Selectable/Weapon/P_FlameThrower.cs
+++ Computer Virus Survivors/Assets/Scripts/Selectable/Weapon/P_FlameThrower.cs
@@ -10,7 +10,6 @@ public class P_FlameThrower : ProjectileBehaviour
     [SerializeField] private float tick = 0.5f;
     private ParticleSystem flameParticle;
 
-    private Coroutine damageCoroutine;
     private float radius;
 
     public void Initialize(int damage, float radius)
@@ -21,14 +20,30 @@ public class P_FlameThrower : ProjectileBehaviour
 
     public void FireOn(float duration)
     {
-        damageCoroutine = StartCoroutine(GiveDamage());
         StartCoroutine(Fire(duration));
     }
 
-    private IEnumerator GiveDamage()
+
+    private IEnumerator Fire(float duration)
     {
-        while (true)
+        if (flameParticle == null)
         {
+            flameParticle = GetComponent<ParticleSystem>();
+        }
+        var shape = flameParticle.shape;
+        shape.angle = fireAngle / 2;
+        var main = flameParticle.main;
+        main.duration = duration;
+        main.startLifetime = 0.5f * radius / 3;
+        var emission = flameParticle.emission;
+        emission.rateOverTime = 200 * Mathf.Pow(radius / 3, 2);
+        flameParticle.Play();
+
+        float elapsedTime = 0;
+
+        while (elapsedTime < duration)
+        {
+
             // 부채꼴 범위의 바이러스에게 데미지를 줌
             Vector3 transformOnPlane = new Vector3(transform.position.x, 0, transform.position.z);
             Collider[] colliders = Physics.OverlapSphere(transformOnPlane, radius, virusLayer);
@@ -42,22 +57,9 @@ public class P_FlameThrower : ProjectileBehaviour
                 }
             }
             yield return new WaitForSeconds(tick);
+            elapsedTime += tick;
         }
-    }
 
-    private IEnumerator Fire(float duration)
-    {
-        if (flameParticle == null)
-        {
-            flameParticle = GetComponent<ParticleSystem>();
-        }
-        var shape = flameParticle.shape;
-        shape.angle = fireAngle / 2;
-        var main = flameParticle.main;
-        main.duration = duration;
-        flameParticle.Play();
-        yield return new WaitForSeconds(duration);
-        StopCoroutine(damageCoroutine);
     }
 
     protected override void OnTriggerEnter(Collider other)
