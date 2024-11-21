5a82bb8d3b0930de92ef386f766deb282b8d0c2c
Nyso97 <justin0907@naver.com>
2024-11-21 21:09:11 +0900

    방패 구현
    레벨업 사항 추가
    tick부분 삭제

--- Computer Virus Survivors/Assets/Scripts/Selectable/Weapon/P_Shield.cs
+++ Computer Virus Survivors/Assets/Scripts/Selectable/Weapon/P_Shield.cs
@@ -5,11 +5,9 @@ using UnityEngine;
 
 public class P_Shield : ProjectileBehaviour
 {
-    [SerializeField] private float tick = 0.5f;
-    [SerializeField] private float spinSpeed = 90.0f;
+    [SerializeField] private float spinSpeed;
     private float currentAngle;
     private float rotateSpeed;
-    private bool canDamage = true;
 
     // Angle은 0 이상일 경우만 초기화
     public void Initialize(int damage, float angle, float rotateSpeed)
@@ -38,17 +36,9 @@ public class P_Shield : ProjectileBehaviour
 
     protected override void OnTriggerEnter(Collider other)
     {
-        if (canDamage && other.CompareTag("Virus"))
+        if (other.CompareTag("Virus"))
         {
             other.GetComponent<VirusBehaviour>().GetDamage(damage);
-            StartCoroutine(CoolDown());
         }
-    }
-
-    private IEnumerator CoolDown()
-    {
-        canDamage = false;
-        yield return new WaitForSeconds(tick);
-        canDamage = true;
     }
 }
--- Computer Virus Survivors/Assets/Scripts/Selectable/Weapon/W_Shield.cs
+++ Computer Virus Survivors/Assets/Scripts/Selectable/Weapon/W_Shield.cs
@@ -35,26 +35,27 @@ public class W_Shield : WeaponBehaviour
                 BasicMultiProjectile += 1;
                 break;
             case 3:
-                BasicAttackPeriod *= (1 - 0.3f) / 1;
+                BasicAttackRange *= (1 + 0.25f) / 1;
+                BasicAttackPeriod *= 1 / (1 + 0.3f);
                 break;
             case 4:
-                BasicDamage += 20;
+                BasicDamage += 10;
                 break;
             case 5:
                 BasicMultiProjectile += 1;
                 break;
             case 6:
-                BasicAttackPeriod *= (1 - 0.3f) / 1;
+                BasicAttackRange *= (1 + 0.25f) / 1;
+                BasicAttackPeriod *= 1 / (1 + 0.3f);
                 break;
             case 7:
-                BasicDamage += 40;
+                BasicDamage += 10;
                 break;
             case 8:
                 BasicMultiProjectile += 1;
                 break;
             case 9:
-                BasicMultiProjectile += 1;
-                BasicDamage += 40;
+                BasicMultiProjectile += 2;
                 break;
             default:
                 break;
@@ -66,31 +67,31 @@ public class W_Shield : WeaponBehaviour
         switch (MaxLevel)
         {
             case 1:
-                explanations[0] = "플레이어 주위의 랜덤한 위치에 코어가 떨어집니다.\n떨어진 위치 주변의 바이러스들은 피해를 입습니다.";
+                explanations[0] = "백신 방패가 플레이어 주위를 회전하면서 닿은 바이러스에게 데미지를 입힙니다";
                 break;
             case 2:
-                explanations[1] = "떨어지는 코어 1개 추가";
+                explanations[1] = "백신 방패 1개 추가";
                 goto case 1;
             case 3:
-                explanations[2] = "공격 주기 30% 감소";
+                explanations[2] = "공격 범위 25% 증가, 회전 속도 30% 증가";
                 goto case 2;
             case 4:
-                explanations[3] = "기본 데미지 20 증가";
+                explanations[3] = "기본 데미지 10 증가";
                 goto case 3;
             case 5:
-                explanations[4] = "떨어지는 코어 1개 추가";
+                explanations[4] = "백신 방패 1개 추가";
                 goto case 4;
             case 6:
-                explanations[5] = "공격 주기 30% 감소";
+                explanations[5] = "공격 범위 25% 증가, 회전 속도 30% 증가";
                 goto case 5;
             case 7:
-                explanations[6] = "기본 데미지 40 증가";
+                explanations[6] = "기본 데미지 10 증가";
                 goto case 6;
             case 8:
-                explanations[7] = "떨어지는 코어 1개 추가";
+                explanations[7] = "백신 방패 1개 추가";
                 goto case 7;
             case 9:
-                explanations[8] = "떨어지는 코어 1개 증가, 기본 데미지 40 증가";
+                explanations[8] = "백신 방패 2개 추가";
                 goto case 8;
         }
     }
