2b91e6ac7076dcc8286faf0b53615723e97911ac
EsOTi <tpwls6630@naver.com>
2024-12-05 23:43:50 +0900

    fix : 바이러스 스폰 이펙트 보완
    - 스폰 이펙트의 사이즈를 조절할 수 있습니다. 변수명은 defaultEffectSize입니다.
    - 바이러스의 사이즈를 반환하는 함수를 만들었습니다. 바이러스의 콜라이더를 기준으로 값을 계산합니다.
    - VirusSpawnFactory에서 이펙트를 생성할 때 바이러스의 사이즈가 필요하므로 이펙트보다 바이러스를 먼저 만들게 됩니다. 이때문에 스폰 로직이 변경되었습니다.
    - 스폰 이펙트의 메터리얼을 수정하였습니다. 기존의 3x3x3모양의 큐브보다 1x1x1모양의 박스가 더 이뻐보입니다.

--- Computer Virus Survivors/Assets/Scripts/Virus/VirusSpawnEffect.cs
+++ Computer Virus Survivors/Assets/Scripts/Virus/VirusSpawnEffect.cs
@@ -4,8 +4,38 @@ using UnityEngine;
 
 public class VirusSpawnEffect : MonoBehaviour
 {
+
+    private new ParticleSystem particleSystem;
+    private float effectSize;
+    [SerializeField] private float defaultEffectSize = 0.7f;
+    private void Awake()
+    {
+        particleSystem = GetComponent<ParticleSystem>();
+        effectSize = defaultEffectSize;
+    }
+
+    public void SetEffectSize(float size)
+    {
+        effectSize = size;
+    }
+
+    public void SetVirusSize(float virusSize)
+    {
+        particleSystem.transform.localScale = Vector3.one * virusSize * effectSize;
+        transform.position = new Vector3(transform.position.x, virusSize / 2f, transform.position.z);
+    }
+
+    public WaitForSeconds PlayAndWaitUntilEffectPeak()
+    {
+        float spawnDuration = particleSystem.main.duration;
+
+        return new WaitForSeconds(spawnDuration / 2f);
+    }
+
     private void OnParticleSystemStopped()
     {
+        effectSize = defaultEffectSize;
         PoolManager.instance.ReturnObject(PoolType.Virus_SpawnEffect, gameObject);
     }
+
 }
