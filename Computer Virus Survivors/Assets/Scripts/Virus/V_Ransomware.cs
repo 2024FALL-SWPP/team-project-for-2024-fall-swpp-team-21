e79b7f1f04cef948427a39cd0a41ad2d8ba8c64d
Nyso97 <justin0907@naver.com>
2024-12-15 00:39:29 +0900

    v_ransomware animation

--- Computer Virus Survivors/Assets/Scripts/Virus/V_Ransomware.cs
+++ Computer Virus Survivors/Assets/Scripts/Virus/V_Ransomware.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 
 public class V_Ransomware : VirusBehaviour
 {
+    [SerializeField] private Animator animator;
     [SerializeField] private float attackPeriod = 10.0f;
     [SerializeField] private float attackDelay = 0.5f;
 
@@ -90,6 +91,7 @@ public class V_Ransomware : VirusBehaviour
         {
             yield return new WaitForSeconds(attackPeriod);
             startAttack = true;
+            animator.SetTrigger("t_Attack");
             yield return new WaitForSeconds(attackDelay);
             startAttack = false;
             attackActions[UnityEngine.Random.Range(0, attackActions.Count)]();
