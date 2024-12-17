f6fa57a3e18240bac3814965b61ed9825bd7d40b
EsOTi <tpwls6630@naver.com>
2024-12-17 09:52:34 +0900

    애드웨어 공격 사운드 구현

--- Computer Virus Survivors/Assets/Scripts/SFXPreset.cs
+++ Computer Virus Survivors/Assets/Scripts/SFXPreset.cs
@@ -7,13 +7,26 @@ public class SFXPreset : ScriptableObject
     [Header("공격 이팩트 사운드(동시)")]
     [SerializeField] private AudioClip[] sfxClips;
 
+    [Header("바이러스용 사운드")]
+    [SerializeField] private bool isVirus;
+
     public AudioClip[] SfxClips => sfxClips;
 
     public void Play()
     {
-        foreach (var audioClip in sfxClips)
+        if (isVirus)
         {
-            SFXManager.instance.PlaySound(audioClip);
+            foreach (var audioClip in sfxClips)
+            {
+                SFXManager.instance.PlaySound_Virus(audioClip);
+            }
+        }
+        else
+        {
+            foreach (var audioClip in sfxClips)
+            {
+                SFXManager.instance.PlaySound(audioClip);
+            }
         }
     }
 }
