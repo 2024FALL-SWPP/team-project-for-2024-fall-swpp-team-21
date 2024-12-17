0000a5b0b5b6e9b9e784c78cc2222e4f57013c6c
Nyso97 <justin0907@naver.com>
2024-12-17 18:32:59 +0900

    Wall

--- Computer Virus Survivors/Assets/Scripts/MapBoundary.cs
+++ Computer Virus Survivors/Assets/Scripts/MapBoundary.cs
@@ -19,7 +19,7 @@ public class MapBoundary : Singleton<MapBoundary>
         SetBoundary();
     }
 
-    public void SetBoundary()
+    private void SetBoundary()
     {
         Vector2 leftPoint = new Vector2(borderPoints[0].position.x, borderPoints[0].position.z);
         Vector2 rightPoint = new Vector2(borderPoints[1].position.x, borderPoints[1].position.z);
@@ -29,17 +29,6 @@ public class MapBoundary : Singleton<MapBoundary>
         border2DPoints = new Vector2[] { leftPoint, upPoint, rightPoint, downPoint };
         rightUpDistance = Vector2.Distance(leftPoint, upPoint);
         rightDownDistance = Vector2.Distance(leftPoint, downPoint);
-
-        SetTransform(walls[0], leftPoint, upPoint);
-        SetTransform(walls[1], leftPoint, downPoint);
-        SetTransform(walls[2], rightPoint, upPoint);
-        SetTransform(walls[3], rightPoint, downPoint);
-    }
-
-    private void SetTransform(GameObject wall, Vector2 point1, Vector2 point2)
-    {
-        wall.transform.position = new Vector3((point1.x + point2.x) / 2, wall.transform.position.y, (point1.y + point2.y) / 2);
-        wall.transform.localScale = new Vector3(Vector2.Distance(point1, point2), wall.transform.localScale.y, wall.transform.localScale.z);
     }
 
     public Vector2 GetRandomPoint()
@@ -70,22 +59,3 @@ public class MapBoundary : Singleton<MapBoundary>
         return a.x * b.y - a.y * b.x;
     }
 }
-
-
-#if UNITY_EDITOR
-[UnityEditor.CustomEditor(typeof(MapBoundary))]
-public class MapBoundaryEditor : UnityEditor.Editor
-{
-    public override void OnInspectorGUI()
-    {
-        base.OnInspectorGUI();
-
-        if (GUILayout.Button("Set Boundary"))
-        {
-            MapBoundary mapBoundary = target as MapBoundary;
-            mapBoundary.SetBoundary();
-            Debug.Log("Successfully set boundary");
-        }
-    }
-}
-#endif
